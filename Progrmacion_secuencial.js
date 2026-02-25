11c83e0d9c9344e0bf3eaa8356fbf12e64115907
Melisa-arango <melisa.bustamante06@gmail.com>
2026-02-25 16:05:21 -0500

    operaciones

---
+++ Progrmacion_secuencial.js
@@ -0,0 +1,17 @@
+let nombre = "Santy";
+let edad = 23;
+let apellido = "osorno";
+let numero = 6; 
+function sumar(a, b) {
+    return a + b;
+}
+function restar(a, b) {
+    let operacion = a - b;
+    return operacion;
+}
+console.log ("El resultado de la suma es: " + sumar (numero, edad));
+let ResultadoResta = restar (5, 7);
+console.log ("Resultado de la resta: ") + ResultadoResta;
+console.log ("nombre completo: " + nombre + " " + apellido + " , edad " + edad);
+
+
