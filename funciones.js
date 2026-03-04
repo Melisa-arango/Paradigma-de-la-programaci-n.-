b1b4a43fe052f0150e9c09e028bffc47a8b49fa7
Melisa-arango <melisa.bustamante06@gmail.com>
2026-03-04 14:58:51 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,32 @@
+function Suma(x) {
+    let accion = x + 5;
+    return accion;
+
+};
+console.log(Suma(7));
+
+function Dividir(x) {
+     let accion = x/10;
+     return accion;
+
+};
+console.log(Dividir(10));
+
+let Resultado=0;
+function sumarPrecios(a, b) {
+    return Resultado = Resultado + a * b;
+
+};
+
+console.log(sumarPrecios(4, 6));
+console.log(sumarPrecios(2, 3));
+console.log(sumarPrecios(5, 8));
+
+function Multiplicar(a) {
+    return a * a;
+
+};
+function SumaDeLaMultiplicacion(num1, num2) {
+    return Multiplicar(num1) + Multiplicar(num2); 
+};
+console.log(SumaDeLaMultiplicacion(3, 4));
