8968cda6afb457628dff53d6f1087a084a71328b
Melisa-arango <melisa.bustamante06@gmail.com>
2026-03-12 18:28:14 -0500

    yeison

---
+++ Backend/Mi_primer_servidor.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express ();
+
+app.get('/', (req, res) => {
+    res.send('hola mundo');
+});
+
+app.get('/json' , (req, res) => {
+    res.json({mensaje: 'hola mundo'});
+});
+
+app.listen(3000, () => {
+    console.log('servidor escuchando en el puerto 3000');
+
+});
